faba19ff2d1a70e7b89ecbaf917c299b086b248b
xiaolongbao74 <kaneko@tokaiseitosolutions.jp>
2025-11-19 13:59:20 +0900

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, Navigate, useLocation } from 'react-router-dom'
 import UserKoubanhyou from './components/UserKoubanhyou'
 import UserSchedule from './components/UserSchedule'
 import AdminMembers from './components/AdminMembers'
@@ -6,20 +6,33 @@ import AdminSongs from './components/AdminSongs'
 import AdminKoubanhyou from './components/AdminKoubanhyou'
 import AdminSchedule from './components/AdminSchedule'
 
+function Navigation() {
+  const location = useLocation()
+  const isAdminPage = location.pathname.startsWith('/admin')
+
+  if (isAdminPage) {
+    // 管理画面ではナビゲーションを表示しない
+    return null
+  }
+
+  return (
+    <nav className="nav">
+      <div className="nav-container">
+        <h1>トムは真夜中の庭で～ハイライト版～</h1>
+        <ul className="nav-links">
+          <li><Link to="/koubanhyou">香盤表</Link></li>
+          <li><Link to="/schedule">スケジュール</Link></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
-        <nav className="nav">
-          <div className="nav-container">
-            <h1>トムは真夜中の庭で～ハイライト版～</h1>
-            <ul className="nav-links">
-              <li><Link to="/koubanhyou">香盤表</Link></li>
-              <li><Link to="/schedule">スケジュール</Link></li>
-              <li><Link to="/admin/members">管理</Link></li>
-            </ul>
-          </div>
-        </nav>
+        <Navigation />
         
         <Routes>
           <Route path="/" element={<Navigate to="/koubanhyou" replace />} />
